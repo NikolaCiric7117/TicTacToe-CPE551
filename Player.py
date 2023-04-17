bb75b902c55f99cae2cde62f442583aa40de25cf
NikolaCiric7117 <nciric@stevens.edu>
2023-04-17 15:19:48 -0400

    Finished Player class

--- Player.py
+++ Player.py
@@ -1,5 +1,5 @@
 class Player():
-  #player class, does nothing but just sets the name and symbol
+  #player class, does nothing but just sets the name and symbol which is used in other classes
   def __init__(self,name,symbol):
     self.name = name
     self.symbol = symbol
